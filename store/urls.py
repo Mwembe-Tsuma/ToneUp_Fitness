ad368b291a5ea306a5eb1a940119a0fb133c5691
Mwembe-Tsuma <mwembeemmanuel@gmail.com>
2024-03-25 16:11:07 +0300

    urls.py

---
+++ store/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+	#Leave as empty string for base url
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+	path('update_item/', views.updateItem, name="update_item"),
+	path('process_order/', views.processOrder, name="process_order"),
+    path("register/", views.register, name="register"),
+    path("login/", views.Login, name="login"),
+    path("logout/", views.Logout, name="logout"),
+    path("change_password/", views.change_password, name="change_password"),
+    path("loggedin_contact/", views.loggedin_contact, name="loggedin_contact"),
+]
